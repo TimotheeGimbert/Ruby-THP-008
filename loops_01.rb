c7787f55b8da5f4b2c83d210badffc7ccbf53771
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-04 11:37:34 +0200

    Loop exercices

---
+++ loops_01.rb
@@ -0,0 +1,9 @@
+puts "Hi"
+puts "Hi"  
+puts "Hi"  
+puts "Hi"  
+puts "hi"
+
+5.times do
+  puts "Hello"
+end
---
+++ loops_02.rb
@@ -0,0 +1,11 @@
+5.times do |index|
+  puts index
+end
+
+5.times do |testing|
+  puts testing
+end
+
+5.times do |i|
+  puts i
+end
